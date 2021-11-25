3cdbd2ed2a3c67bae55d65a304eaecbba25c71bf
debych <zh.eliseev@yandex.ru>
2021-11-25 12:52:23 +0300

    [Tokenomics] add enter leave staking events

--- src/tokenomics/contracts/AlgebraTokenStaking.sol
+++ src/tokenomics/contracts/AlgebraTokenStaking.sol
@@ -9,8 +9,11 @@ contract AlgebraTokenStaking is FreezableToken{
     using SafeMath for uint256;
 
     uint256 public freezeTime = 1800;
+    address public owner;
     IERC20Minimal public ALGB;
-    address owner;
+
+    event Entered(address staker, uint256 ALGBAmount, uint256 xALGBAmount);
+    event Left(address staker, uint256 xALGBAmount, uint256 ALGBAmount);
 
     modifier onlyOwner(){
         require(msg.sender == owner, 'only owner can call this');
@@ -37,17 +40,19 @@ contract AlgebraTokenStaking is FreezableToken{
         uint256 totalALGB = ALGB.balanceOf(address(this));
         // Gets the amount of xALGB in existence
         uint256 totalShares = totalSupply();
+        uint256 what;
         // If no xALGB exists, mint it 1:1 to the amount put in
         if (totalShares == 0 || totalALGB == 0) {
             mintAndFreeze(msg.sender, _amount, uint64(block.timestamp + freezeTime));
         }
         // Calculate and mint the amount of xALGB the ALGB is worth. The ratio will change overtime, as xALGB is burned/minted and ALGB deposited + gained from fees / withdrawn.
         else {
-            uint256 what = _amount.mul(totalShares).div(totalALGB);
+            what = _amount.mul(totalShares).div(totalALGB);
             mintAndFreeze(msg.sender, what, uint64(block.timestamp + freezeTime));
         }
         // Lock the ALGB in the contract
         ALGB.transferFrom(msg.sender, address(this), _amount);
+        emit Entered(msg.sender, _amount, what);
     }
 
     // Leave the bar. Claim back your ALGBs.
@@ -59,6 +64,7 @@ contract AlgebraTokenStaking is FreezableToken{
         uint256 what = _share.mul(ALGB.balanceOf(address(this))).div(totalShares);
         _burn(msg.sender, _share);
         ALGB.transfer(msg.sender, what);
+        emit Left(msg.sender, _share, what);
     }
 
     function currentBalance(uint256 _amount) external view returns(uint256){
