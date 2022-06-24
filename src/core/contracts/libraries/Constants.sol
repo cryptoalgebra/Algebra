3aea19cb92afd5cb72834899e66176273b291b5c
IliaAzhel <aje-ilia2013@yandex.by>
2022-06-24 15:43:46 +0300

    [Core] document BASE_FEE number format (145)

--- src/core/contracts/libraries/Constants.sol
+++ src/core/contracts/libraries/Constants.sol
@@ -5,6 +5,7 @@ library Constants {
   uint8 internal constant RESOLUTION = 96;
   uint256 internal constant Q96 = 0x1000000000000000000000000;
   uint256 internal constant Q128 = 0x100000000000000000000000000000000;
+  // fee value in hundredths of a bip, i.e. 1e-6
   uint16 internal constant BASE_FEE = 100;
   int24 internal constant TICK_SPACING = 60;
 
