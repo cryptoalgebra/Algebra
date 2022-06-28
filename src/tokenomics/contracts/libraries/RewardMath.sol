94b9ba53628b6259f939998133ffcc1c7e421e79
IliaAzhel <aje-ilia2013@yandex.by>
2022-06-28 16:17:43 +0300

    [Tokenomics] add comments to RewardMath

--- src/tokenomics/contracts/libraries/RewardMath.sol
+++ src/tokenomics/contracts/libraries/RewardMath.sol
@@ -8,27 +8,30 @@ import '@openzeppelin/contracts/math/Math.sol';
 /// @notice Allows computing rewards given some parameters of farms and incentives
 library RewardMath {
     /// @notice Compute the amount of rewards owed given parameters of the incentive and farm
-    /// @param totalReward The total amount of unclaimed rewards left for an incentive
-    /// @param startTime When the incentive rewards began in epoch seconds
+    /// @param totalReward The total amount of rewards
+    /// @param initTime Start of the incentive rewards distribution
     /// @param endTime When rewards are no longer being dripped out in epoch seconds
     /// @param liquidity The amount of liquidity, assumed to be constant over the period over which the snapshots are measured
+    /// @param totalLiquidity The amount of liquidity of all positions participating in the incentive
     /// @param secondsPerLiquidityInsideX128 The seconds per liquidity of the liquidity tick range as of the current block timestamp
     /// @return reward The amount of rewards owed
     function computeRewardAmount(
         uint256 totalReward,
-        uint256 startTime,
+        uint256 initTime,
         uint256 endTime,
         uint128 liquidity,
         uint224 totalLiquidity,
         uint160 secondsPerLiquidityInsideX128
     ) internal pure returns (uint256 reward) {
-        // this operation is safe, as the difference cannot be greater than 1/farm.liquidity
+        // this operation is safe, as the difference cannot be greater than 1/deposit.liquidity
         uint256 secondsInsideX128 = secondsPerLiquidityInsideX128 * liquidity;
 
-        if (endTime - startTime == 0) {
+        if (endTime - initTime == 0) {
+            // liquidity <= totalLiquidity
             reward = FullMath.mulDiv(totalReward, liquidity, totalLiquidity);
         } else {
-            reward = FullMath.mulDiv(totalReward, secondsInsideX128, (endTime - startTime) << 128);
+            // reward less than uint256, as secondsInsideX128 cannot be greater (than endTime - initTime) << 128
+            reward = FullMath.mulDiv(totalReward, secondsInsideX128, (endTime - initTime) << 128);
         }
     }
 }
