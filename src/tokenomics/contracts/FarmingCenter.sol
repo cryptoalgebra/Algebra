8d5a3b33a6b44ce6902a487ed0405d38adc7ccd9
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-07-04 18:19:17 +0300

    [Tokenomics] remove unused imports from FC

--- src/tokenomics/contracts/FarmingCenter.sol
+++ src/tokenomics/contracts/FarmingCenter.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity =0.7.6;
 pragma abicoder v2;
-import './farmings/incentiveFarming/interfaces/IAlgebraIncentiveVirtualPool.sol';
-import './farmings/eternalFarming/interfaces/IAlgebraEternalVirtualPool.sol';
 import './interfaces/IFarmingCenter.sol';
 import './interfaces/IFarmingCenterVault.sol';
 
