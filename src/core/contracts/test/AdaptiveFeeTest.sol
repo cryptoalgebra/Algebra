f6c1682faa2d95e61acb2f869d80e5811e8f5dc6
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-07-06 14:25:48 +0300

    [Core] update AdaptiveFeeTest

--- src/core/contracts/test/AdaptiveFeeTest.sol
+++ src/core/contracts/test/AdaptiveFeeTest.sol
@@ -7,15 +7,15 @@ import '../libraries/Constants.sol';
 contract AdaptiveFeeTest {
   AdaptiveFee.Configuration public feeConfig =
     AdaptiveFee.Configuration(
-      3000 - 500, // alpha1
-      10000 - 3000, // alpha2
-      150, // beta1
-      1500, // beta2
-      30, // gamma1
-      100, // gamma2
-      30, // volumeBeta
+      3000 - Constants.BASE_FEE, // alpha1
+      15000 - 3000, // alpha2
+      360, // beta1
+      60000, // beta2
+      59, // gamma1
+      8500, // gamma2
+      0, // volumeBeta
       10, // volumeGamma
-      Constants.BASE_FEE
+      Constants.BASE_FEE // baseFee
     );
 
   function getFee(uint88 volatility, uint256 volumePerLiquidity) external view returns (uint256 fee) {
