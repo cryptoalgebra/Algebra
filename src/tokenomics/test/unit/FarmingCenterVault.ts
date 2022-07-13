f661bf12b0341b4cac5089a26a2d1208be96824d
IliaAzhel <aje-ilia2013@yandex.by>
2022-07-13 16:32:25 +0300

    [Tokenomics] add tests for farming center vault

---
+++ src/tokenomics/test/unit/FarmingCenterVault.ts
@@ -0,0 +1,422 @@
+import { BigNumber } from 'ethers'
+import { LoadFixtureFunction } from '../types'
+import { algebraFixture, mintPosition, AlgebraFixtureType } from '../shared/fixtures'
+import {
+  expect,
+  getMaxTick,
+  getMinTick,
+  FeeAmount,
+  TICK_SPACINGS,
+  blockTimestamp,
+  BNe18,
+  ActorFixture,
+  makeTimestamps,
+} from '../shared'
+import { createFixtureLoader, provider } from '../shared/provider'
+import { HelperCommands, ERC20Helper} from '../helpers'
+import { ContractParams } from '../../types/contractParams'
+import { HelperTypes } from '../helpers/types'
+
+let loadFixture: LoadFixtureFunction
+const LIMIT_FARMING = true;
+const ETERNAL_FARMING = false;
+
+describe('unit/FarmingCenterVault', () => {
+  const actors = new ActorFixture(provider.getWallets(), provider)
+  const lpUser0 = actors.lpUser0()
+  const amountDesired = BNe18(10)
+  const amountLocked = BigNumber.from(1000)
+  const totalReward = BNe18(100)
+  const bonusReward = BNe18(100)
+  const erc20Helper = new ERC20Helper()
+  let helpers: HelperCommands
+  let context: AlgebraFixtureType
+  let timestamps: ContractParams.Timestamps
+  let eternalTimestamps: ContractParams.Timestamps
+  let tokenId: string
+
+  before('loader', async () => {
+    loadFixture = createFixtureLoader(provider.getWallets(), provider)
+  })
+
+  beforeEach('create fixture loader', async () => {
+    context = await loadFixture(algebraFixture)
+    helpers = HelperCommands.fromTestContext(context, actors, provider)
+  })
+
+  describe('#enter/exit farming', () => {
+    
+    let incentiveId: string
+    let eternalFarmingId: string
+    let incentiveArgs: HelperTypes.CreateIncentive.Args
+    let eternalFarmingArgs: HelperTypes.CreateIncentive.Args
+
+    beforeEach(async () => {
+      
+      /** We will be doing a lot of time-testing here, so leave some room between
+        and when the incentive starts */
+      timestamps = makeTimestamps(1_000 + (await blockTimestamp()))
+      eternalTimestamps = makeTimestamps(1_001 + (await blockTimestamp()))
+
+      await erc20Helper.ensureBalancesAndApprovals(
+        lpUser0,
+        [context.token0, context.token1],
+        amountDesired,
+        context.nft.address
+      )
+
+      tokenId = await mintPosition(context.nft.connect(lpUser0), {
+        token0: context.token0.address,
+        token1: context.token1.address,
+        fee: FeeAmount.MEDIUM,
+        tickLower: getMinTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        tickUpper: getMaxTick(TICK_SPACINGS[FeeAmount.MEDIUM]),
+        recipient: lpUser0.address,
+        amount0Desired: amountDesired,
+        amount1Desired: amountDesired,
+        amount0Min: 0,
+        amount1Min: 0,
+        deadline: (await blockTimestamp()) + 1000,
+      })
+
+      await context.nft
+        .connect(lpUser0)
+        ['safeTransferFrom(address,address,uint256)'](lpUser0.address, context.farmingCenter.address, tokenId)
+
+      incentiveArgs = {
+        rewardToken: context.rewardToken,
+        bonusRewardToken: context.bonusRewardToken,
+        totalReward,
+        bonusReward,
+        poolAddress: context.poolObj.address,
+        ...timestamps,
+      }
+
+      eternalFarmingArgs = {
+        rewardToken: context.rewardToken,
+        bonusRewardToken: context.bonusRewardToken,
+        totalReward,
+        bonusReward,
+        poolAddress: context.poolObj.address,
+        ...eternalTimestamps,
+        eternal: true,
+        rewardRate: BigNumber.from('10'),
+        bonusRewardRate: BigNumber.from('50')
+      }
+
+      await erc20Helper.ensureBalancesAndApprovals(
+        lpUser0,
+        context.rewardToken,
+        amountDesired.mul(BigNumber.from(2)),
+        context.nft.address
+      )
+
+      incentiveId = await helpers.getIncentiveId(await helpers.createIncentiveWithMultiplierFlow(incentiveArgs))
+
+      eternalFarmingId = await helpers.getIncentiveId(await helpers.createIncentiveWithMultiplierFlow(eternalFarmingArgs))
+
+
+    })
+
+    describe('limit farming', () => {
+
+      let vaultBalanceBeforeEnter: BigNumber
+
+      beforeEach('enter limit farming', async () => {
+
+        vaultBalanceBeforeEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+
+        await erc20Helper.ensureBalancesAndApprovals(
+            lpUser0,
+            context.rewardToken,
+            amountLocked,
+            context.farmingCenter.address
+          )
+        
+        await context.farmingCenter.connect(lpUser0).enterFarming(
+          {
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          amountLocked,
+          LIMIT_FARMING
+        )
+
+      })
+
+      it('locked tokens were sent to vault', async () => {
+        let vaultBalanceAfterEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+        expect(vaultBalanceAfterEnter.sub(vaultBalanceBeforeEnter)).to.eq(amountLocked)
+      })
+
+      it('locked tokens were returned to user', async () => {
+        let vaultBalanceBeforeExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+        let userBalanceBeforeExit = await context.rewardToken.balanceOf(lpUser0.address)
+
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          LIMIT_FARMING
+        )
+
+        let vaultBalanceAfterExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+        let userBalanceAfterExit = await context.rewardToken.balanceOf(lpUser0.address)
+
+        expect(vaultBalanceBeforeExit.sub(vaultBalanceAfterExit)).to.eq(amountLocked)
+        expect(userBalanceAfterExit.sub(userBalanceBeforeExit)).to.eq(amountLocked)
+
+      })
+
+      it('is balances correct returns value after enter', async () => {
+        let balance = await context.farmingCenterVault.balances(tokenId, incentiveId)
+
+        expect(balance).to.eq(amountLocked)
+      })
+
+      it('is balances correct returns value after exit', async () => {
+        
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          LIMIT_FARMING
+        )
+
+        let balance = await context.farmingCenterVault.balances(tokenId, incentiveId)
+
+        expect(balance).to.eq(BigNumber.from(0))
+      })
+
+    })
+
+    describe('eternal farming', () => {
+
+        let vaultBalanceBeforeEnter: BigNumber
+
+        beforeEach('enter eternal farming', async () => {
+  
+          vaultBalanceBeforeEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+  
+          await erc20Helper.ensureBalancesAndApprovals(
+              lpUser0,
+              context.rewardToken,
+              amountLocked,
+              context.farmingCenter.address
+            )
+  
+
+          await context.farmingCenter.connect(lpUser0).enterFarming(
+            {
+              
+              pool: context.pool01,
+              rewardToken: context.rewardToken.address,
+              bonusRewardToken: context.bonusRewardToken.address,
+              ...eternalTimestamps,
+            },
+            tokenId,
+            amountLocked,
+            ETERNAL_FARMING
+          )
+  
+        })
+  
+        it('locked tokens were sent to vault', async () => {
+          let vaultBalanceAfterEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address) 
+          expect(vaultBalanceAfterEnter.sub(vaultBalanceBeforeEnter)).to.eq(amountLocked)
+        })
+  
+        it('locked tokens were returned to user', async () => {
+          let vaultBalanceBeforeExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+          let userBalanceBeforeExit = await context.rewardToken.balanceOf(lpUser0.address)
+  
+          await context.farmingCenter.connect(lpUser0).exitFarming(
+            {
+              
+              pool: context.pool01,
+              rewardToken: context.rewardToken.address,
+              bonusRewardToken: context.bonusRewardToken.address,
+              ...eternalTimestamps,
+            },
+            tokenId,
+            ETERNAL_FARMING
+          )
+  
+          let vaultBalanceAfterExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+          let userBalanceAfterExit = await context.rewardToken.balanceOf(lpUser0.address)
+  
+          expect(vaultBalanceBeforeExit.sub(vaultBalanceAfterExit)).to.eq(amountLocked)
+          expect(userBalanceAfterExit.sub(userBalanceBeforeExit)).to.eq(amountLocked)
+  
+        })
+  
+        it('is balances correct returns value after enter', async () => {
+          let balance = await context.farmingCenterVault.balances(tokenId, eternalFarmingId)
+  
+          expect(balance).to.eq(amountLocked)
+        })
+  
+        it('is balances correct returns value after exit', async () => {
+
+          await context.farmingCenter.connect(lpUser0).exitFarming(
+              {
+                
+                pool: context.pool01,
+                rewardToken: context.rewardToken.address,
+                bonusRewardToken: context.bonusRewardToken.address,
+                ...eternalTimestamps,
+              },
+              tokenId,
+              ETERNAL_FARMING
+            )
+
+          let balance = await context.farmingCenterVault.balances(tokenId, eternalFarmingId)
+  
+          expect(balance).to.eq(BigNumber.from(0))
+        })
+  })
+
+  describe('limit and eternal at the same time', async () => {
+    
+    let vaultBalanceBeforeEnter: BigNumber
+
+      beforeEach('enter limit farming', async () => {
+
+        vaultBalanceBeforeEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+
+        await erc20Helper.ensureBalancesAndApprovals(
+            lpUser0,
+            context.rewardToken,
+            amountLocked.mul(BigNumber.from(2)),
+            context.farmingCenter.address
+          )
+
+        await context.farmingCenter.connect(lpUser0).enterFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          amountLocked,
+          LIMIT_FARMING
+        )
+
+        await context.farmingCenter.connect(lpUser0).enterFarming(
+            {
+              
+              pool: context.pool01,
+              rewardToken: context.rewardToken.address,
+              bonusRewardToken: context.bonusRewardToken.address,
+              ...eternalTimestamps,
+            },
+            tokenId,
+            amountLocked,
+            ETERNAL_FARMING
+          )
+
+      })
+
+      it('locked tokens were sent to vault', async () => {
+        let vaultBalanceAfterEnter = await context.rewardToken.balanceOf(context.farmingCenterVault.address) 
+        expect(vaultBalanceAfterEnter.sub(vaultBalanceBeforeEnter)).to.eq(amountLocked.mul(BigNumber.from(2)))
+      })
+
+      it('locked tokens were returned to user', async () => {
+        let vaultBalanceBeforeExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+        let userBalanceBeforeExit = await context.rewardToken.balanceOf(lpUser0.address)
+
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          LIMIT_FARMING
+        )
+
+
+
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...eternalTimestamps,
+          },
+          tokenId,
+          ETERNAL_FARMING
+        )
+
+        let vaultBalanceAfterExit = await context.rewardToken.balanceOf(context.farmingCenterVault.address)
+        let userBalanceAfterExit = await context.rewardToken.balanceOf(lpUser0.address)
+        
+        expect(vaultBalanceBeforeExit.sub(vaultBalanceAfterExit)).to.eq(amountLocked.mul(BigNumber.from(2)))
+        expect(userBalanceAfterExit.sub(userBalanceBeforeExit)).to.eq(amountLocked.mul(BigNumber.from(2)))
+
+      })
+
+      it('is balances correct returns value after enter', async () => {
+        let limitBalance = await context.farmingCenterVault.balances(tokenId, incentiveId)
+        let eternalBalance = await context.farmingCenterVault.balances(tokenId, incentiveId)
+        expect(limitBalance).to.eq(amountLocked)
+        expect(eternalBalance).to.eq(amountLocked)
+      })
+
+      it('is balances correct returns value after exit', async () => {
+
+
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...timestamps,
+          },
+          tokenId,
+          LIMIT_FARMING
+        )
+
+
+
+        await context.farmingCenter.connect(lpUser0).exitFarming(
+          {
+            
+            pool: context.pool01,
+            rewardToken: context.rewardToken.address,
+            bonusRewardToken: context.bonusRewardToken.address,
+            ...eternalTimestamps,
+          },
+          tokenId,
+          ETERNAL_FARMING
+        )
+
+        let balance = await context.farmingCenterVault.balances(tokenId, incentiveId)
+
+        expect(balance).to.eq(BigNumber.from(0))
+      })
+  })
+
+
+  })
+})
