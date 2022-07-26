92f733a7bd21f10c0d1c07ad27ae855b1212c883
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-07-26 12:28:43 +0300

    [Core] fix typo (10)

--- src/core/contracts/base/PoolState.sol
+++ src/core/contracts/base/PoolState.sol
@@ -8,7 +8,7 @@ abstract contract PoolState is IAlgebraPoolState {
   struct GlobalState {
     uint160 price; // The square root of the current price in Q64.96 format
     int24 tick; // The current tick
-    uint16 fee; // The current fee in in hundredths of a bip, i.e. 1e-6
+    uint16 fee; // The current fee in hundredths of a bip, i.e. 1e-6
     uint16 timepointIndex; // The index of the last written timepoint
     uint8 communityFeeToken0; // The community fee represented as a percent of all collected fee in thousandths (1e-3)
     uint8 communityFeeToken1;
