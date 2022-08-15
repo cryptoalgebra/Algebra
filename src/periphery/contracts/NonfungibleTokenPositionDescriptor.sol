2811d0ae45bf84a109ddb0484937b969ec99dd83
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-08-15 15:46:59 +0300

    [Periphery] change chainId in NonfungibleTokenPositionDescriptor

--- src/periphery/contracts/NonfungibleTokenPositionDescriptor.sol
+++ src/periphery/contracts/NonfungibleTokenPositionDescriptor.sol
@@ -88,7 +88,7 @@ contract NonfungibleTokenPositionDescriptor is INonfungibleTokenPositionDescript
         if (token == WNativeToken) {
             return TokenRatioSortOrder.DENOMINATOR;
         }
-        if (chainId == 1) {
+        if (chainId == 137) {
             if (token == USDC) {
                 return TokenRatioSortOrder.NUMERATOR_MOST;
             } else if (token == USDT) {
