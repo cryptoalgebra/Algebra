f691c9f389fa5242f103395048291e58489bc83b
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-09-08 15:37:05 +0300

    [Core] implement elastic fee as percent

--- src/core/contracts/AlgebraFactory.sol
+++ src/core/contracts/AlgebraFactory.sol
@@ -113,7 +113,7 @@ contract AlgebraFactory is IAlgebraFactory {
     emit FeeConfiguration(alpha1, alpha2, beta1, beta2, gamma1, gamma2, volumeBeta, volumeGamma, baseFee);
   }
 
-  bytes32 internal constant POOL_INIT_CODE_HASH = 0x894ed5cead32809213e266a8c8e96e9fd1b4f865f52a8920b0d3113d1cf2a589;
+  bytes32 internal constant POOL_INIT_CODE_HASH = 0x1e5215a2bad4bd803ff996642d4ae2478a0e8f1b496393f6cc4b7d94c7304ada;
 
   /// @notice Deterministically computes the pool address given the factory and PoolKey
   /// @param token0 first token
