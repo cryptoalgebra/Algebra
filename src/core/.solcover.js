150577a81f83ac84fb4e34ee79f748648609ab90
Vladislav Volosnikov <Vladyan18@gmail.com>
2022-09-22 11:54:02 +0300

    [Common] bump hardhat and toolbox

--- src/core/.solcover.js
+++ src/core/.solcover.js
@@ -1,10 +1,11 @@
 const fs = require('fs');
 
 const testContracts = fs.readdirSync("./contracts/test")
-
+const skipFiles = testContracts.map((x) => "test/" + x)
+skipFiles.push("libraries/TickMath.sol"); // TODO remove after fix of https://github.com/sc-forks/solidity-coverage/issues/751
 
 module.exports = {
-    skipFiles: testContracts.map((x) => "test/" + x),
+    skipFiles: skipFiles,
     testfiles: "test/*.ts",
     mocha: {
       grep: "@skip-on-coverage", // Find everything with this tag
