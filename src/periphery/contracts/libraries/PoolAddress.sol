4504fe999de86adad2fd61af5cf21c4ee5709b02
IliaAzhel <aje-ilia2013@yandex.by>
2022-10-20 02:44:53 +0300

    reduce elastic fee to 0 for tick difference less than 5

--- src/periphery/contracts/libraries/PoolAddress.sol
+++ src/periphery/contracts/libraries/PoolAddress.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.5.0;
 /// @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
 /// https://github.com/Uniswap/v3-periphery
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0x5467a57fbd55b4e723cb4b06b4efe02b0ab87643cdcb68e8a5e0c3d0aa2c1aef;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x469fa06f244743336ffbe6fca791e86f3f00fc812837c389de582eb0756aa53a;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
