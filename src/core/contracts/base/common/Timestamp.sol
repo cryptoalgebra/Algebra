a7cc32c1b3e8ec986e4770a6d45854d2f3f101a4
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-03-24 14:53:39 +0300

    [Core] remove unchecked block in Timestamp (47)

--- src/core/contracts/base/common/Timestamp.sol
+++ src/core/contracts/base/common/Timestamp.sol
@@ -5,8 +5,6 @@ abstract contract Timestamp {
   /// @dev This function is created for testing by overriding it.
   /// @return A timestamp converted to uint32
   function _blockTimestamp() internal view virtual returns (uint32) {
-    unchecked {
-      return uint32(block.timestamp); // truncation is desired
-    }
+    return uint32(block.timestamp); // truncation is desired
   }
 }
