758e0ec1793bec0b986e75f6d25cbf2ed5b82160
IliaAzhel <aje-ilia2013@yandex.by>
2023-03-27 13:03:31 +0300

    [Core] fix versions in libraries[98]

--- src/core/contracts/libraries/SafeCast.sol
+++ src/core/contracts/libraries/SafeCast.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity >=0.5.0;
+pragma solidity ^0.5.0 || ^0.6.0 || ^0.7.0 || ^0.8.0;
 
 /// @title Safe casting methods
 /// @notice Contains methods for safely casting between types
