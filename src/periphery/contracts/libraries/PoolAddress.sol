e0d60a5ae2819ed6c255c7b1333e4a2856038a46
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-05-19 17:50:40 +0300

    [Core] fix sanity check for LO

--- src/periphery/contracts/libraries/PoolAddress.sol
+++ src/periphery/contracts/libraries/PoolAddress.sol
@@ -5,7 +5,7 @@ pragma solidity >=0.5.0;
 /// @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
 /// https://github.com/Uniswap/v3-periphery
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xa360004fb86ddf4cd7fe9aa67d0c6a7f7812d9069142659003dc503e1d7d241f;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x15b69bf972c5c2df89dd7772b62e872d4048b3741a214df60be904ec5620d9df;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
