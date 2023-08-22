4380282f2dddd5974b62faab2f6b943c01ebe797
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-08-22 11:42:25 +0300

    [Periphery] fix tests and update snapshots

--- src/periphery/test/shared/externalFixtures.ts
+++ src/periphery/test/shared/externalFixtures.ts
@@ -9,7 +9,7 @@ import {
 import {
   abi as PLUGIN_FACTORY_ABI,
   bytecode as PLUGIN_FACTORY_BYTECODE,
-} from '@cryptoalgebra/plugins/artifacts/contracts/DataStorageFactory.sol/DataStorageFactory.json'
+} from '@cryptoalgebra/plugins/artifacts/contracts/BasePluginV1Factory.sol/BasePluginV1Factory.json'
 import { abi as FACTORY_V2_ABI, bytecode as FACTORY_V2_BYTECODE } from '@uniswap/v2-core/build/UniswapV2Factory.json'
 import { ethers } from 'hardhat'
 import { IAlgebraFactory, IWNativeToken, MockTimeSwapRouter } from '../../typechain'
