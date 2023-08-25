046448cfad521022ef3704fd4cbe0945c0e1d4cb
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-08-25 14:04:39 +0300

    [Plugin] remove redundant pragmas in tests

--- src/plugins/contracts/test/AdaptiveFeeTest.sol
+++ src/plugins/contracts/test/AdaptiveFeeTest.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.20;
-pragma abicoder v2;
 
 import '../base/AlgebraFeeConfiguration.sol';
 import '../libraries/AdaptiveFee.sol';
--- src/plugins/contracts/test/VolatilityOracleTest.sol
+++ src/plugins/contracts/test/VolatilityOracleTest.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity =0.8.20;
-pragma abicoder v2;
 
 import '../libraries/VolatilityOracle.sol';
 
