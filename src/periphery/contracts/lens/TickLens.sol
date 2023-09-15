1d47ca3dd8f2421ae8e4a9ba92ca8529ac938f36
Vladislav Volosnikov <Vladyan18@gmail.com>
2023-09-15 19:40:16 +0300

    [Periphery] tickLens: optimize getNextActiveTicks

--- src/periphery/contracts/lens/TickLens.sol
+++ src/periphery/contracts/lens/TickLens.sol
@@ -69,8 +69,35 @@ contract TickLens is ITickLens {
                 }
                 currentTick = newCurrentTick;
 
-                (liquidityGross, liquidityNet, previousTick, nextTick, , ) = IAlgebraPool(pool).ticks(currentTick);
+                (liquidityGross, liquidityNet, previousTick, nextTick) = _getTick(pool, currentTick);
             }
+        }
+    }
+
+    // prevents memory expansion during staticcall
+    // will use [0x00, 0xC0] memory slots
+    function _getTick(
+        address pool,
+        int24 index
+    ) private view returns (uint128 liquidityGross, int128 liquidityNet, int24 previousTick, int24 nextTick) {
+        assembly {
+            let freeMemoryPointer := mload(0x40) // we will need to restore memory further
+            let slot1 := mload(0x60)
+            let slot2 := mload(0x80)
+            let slot3 := mload(0xA0)
+
+            mstore(0x00, 0xf30dba9300000000000000000000000000000000000000000000000000000000) // "ticks" selector
+            mstore(0x04, index)
+            let success := staticcall(gas(), pool, 0, 0x24, 0, 0xC0)
+            liquidityGross := mload(0)
+            liquidityNet := mload(0x20)
+            previousTick := mload(0x40)
+            nextTick := mload(0x60)
+
+            mstore(0x40, freeMemoryPointer) // restore memory
+            mstore(0x60, slot1)
+            mstore(0x80, slot2)
+            mstore(0xA0, slot3)
         }
     }
 }
