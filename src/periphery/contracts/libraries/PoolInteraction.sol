97af420e6ba329340917c116748c482032c5935c
IliaAzhel <aje-ilia2013@yandex.by>
2024-01-25 14:59:14 +0300

    [Periphery] remove IAlgebraFactory import

--- src/periphery/contracts/libraries/PoolInteraction.sol
+++ src/periphery/contracts/libraries/PoolInteraction.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity =0.8.20;
 
-import '@cryptoalgebra/integral-core/contracts/interfaces/IAlgebraFactory.sol';
 import '@cryptoalgebra/integral-core/contracts/interfaces/IAlgebraPool.sol';
 
 import './PositionKey.sol';
