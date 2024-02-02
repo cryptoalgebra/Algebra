3564cc2446870bfd390c56610d5311eb37bb66b3
Vladislav Volosnikov <Vladyan18@gmail.com>
2024-02-02 12:46:58 +0300

    [Core] provide token0 and token1 to the plugin factory in createPlugin

--- src/plugin/contracts/BasePluginV1Factory.sol
+++ src/plugin/contracts/BasePluginV1Factory.sol
@@ -35,7 +35,7 @@ contract BasePluginV1Factory is IBasePluginV1Factory {
   }
 
   /// @inheritdoc IAlgebraPluginFactory
-  function createPlugin(address pool) external override returns (address) {
+  function createPlugin(address pool, address, address) external override returns (address) {
     require(msg.sender == algebraFactory);
     return _createPlugin(pool);
   }
