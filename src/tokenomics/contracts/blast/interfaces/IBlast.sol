b396c07a1a8b37f98e75ad29dcbd4ea6f209aac6
IliaAzhel <aje-ilia2013@yandex.by>
2024-05-16 16:26:35 +0300

    [Common] remove tokenomics

--- src/tokenomics/contracts/blast/interfaces/IBlast.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.5.0;
-
-// Blast Contract Interface
-interface IBlast {
-  function configureClaimableYield() external;
-  function configureClaimableGas() external;
-  function configureGovernor(address _governor) external;
-}
