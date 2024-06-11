8cc0b773e3ad0ebb4363a9c32cd5f54bb023ddb8
fourlen <oborin.2013@mail.ru>
2024-06-11 18:14:33 +0300

    refactored

--- src/plugin/contracts/base/AlgebraBaseModule.sol
+++ src/plugin/contracts/base/AlgebraBaseModule.sol
@@ -5,7 +5,6 @@ import '@cryptoalgebra/integral-core/contracts/interfaces/pool/IAlgebraPoolState
 
 import '@cryptoalgebra/algebra-modular-hub-v0.8.20/contracts/base/AlgebraModule.sol';
 
-
 abstract contract AlgebraBaseModule is AlgebraModule {
     function _getPoolState() internal view returns (uint160 price, int24 tick, uint16 fee, uint8 pluginConfig) {
         (price, tick, fee, pluginConfig, , ) = IAlgebraPoolState(pool).globalState();
