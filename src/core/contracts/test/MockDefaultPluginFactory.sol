3a1f78c3203ff076aa798136a1059422613a171c
IliaAzhel <aje-ilia2013@yandex.by>
2024-06-13 16:47:48 +0300

    [Common] adapt tests & update snapshots

--- src/core/contracts/test/MockDefaultPluginFactory.sol
+++ src/core/contracts/test/MockDefaultPluginFactory.sol
@@ -9,6 +9,8 @@ import './MockPoolPlugin.sol';
 contract MockDefaultPluginFactory is IAlgebraPluginFactory {
   mapping(address => address) public pluginsForPools;
 
+  function afterCreatePoolHook(address plugin, address pool, address deployer) external override {}
+
   function beforeCreatePoolHook(address pool, address, address, address, address, bytes calldata) external override returns (address plugin) {
     plugin = address(new MockPoolPlugin(pool));
     pluginsForPools[pool] = plugin;
