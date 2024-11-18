6e7c68153c429e0f60763b527557f0af705023b2
IliaAzhel <aje-ilia2013@yandex.by>
2024-11-18 21:55:18 +0300

    [Plugin] add security plugin tests

--- src/plugin/test/AlgebraSecurityPlugin.spec.ts
+++ src/plugin/test/AlgebraSecurityPlugin.spec.ts
@@ -5,7 +5,7 @@ import { expect } from './shared/expect';
 import { securityPluginFixture } from './shared/fixtures';
 import { PLUGIN_FLAGS, encodePriceSqrt, expandTo18Decimals, getMaxTick, getMinTick } from './shared/utilities';
 
-import { MockPool, AlgebraSecurityPlugin, SecurityPluginFactory, SecurityRegistry } from '../typechain';
+import { MockPool, AlgebraSecurityPlugin, SecurityPluginFactory, SecurityRegistry, MockFactory } from '../typechain';
 
 import snapshotGasCost from './shared/snapshotGasCost';
 
@@ -16,7 +16,7 @@ describe('AlgebaraSecurityPlugin', () => {
   let mockPool: MockPool; // mock of AlgebraPool
   let pluginFactory: SecurityPluginFactory;
   let registry: SecurityRegistry;
-
+  let mockFactory: MockFactory;
   async function initializeAtZeroTick(pool: MockPool) {
     await pool.initialize(encodePriceSqrt(1, 1));
   }
@@ -26,7 +26,7 @@ describe('AlgebaraSecurityPlugin', () => {
   });
 
   beforeEach('deploy test AlgebaraSecurityPlugin', async () => {
-    ({ plugin, mockPool, pluginFactory, registry } = await loadFixture(securityPluginFixture));
+    ({ plugin, mockPool, pluginFactory, registry, mockFactory } = await loadFixture(securityPluginFixture));
   });
 
   // plain tests for hooks functionality
@@ -78,14 +78,134 @@ describe('AlgebaraSecurityPlugin', () => {
   });
 
   describe('#SecurityPlugin', () => {
+    let defaultConfig: bigint;
 
+    beforeEach('initialize pool', async () => {
+      defaultConfig = await plugin.defaultPluginConfig();
+      await mockPool.setPlugin(plugin);
+      await mockPool.initialize(encodePriceSqrt(1, 1));
+    });
+
+    describe('ENABLE status', async () => {
+      it('works correct', async () => {
+        await expect(mockPool.swapToTick(10)).to.not.be.reverted;
+        await expect(mockPool.mint(wallet.address, wallet.address, 0, 60, 100, '0x')).not.to.be.reverted;
+        await expect(mockPool.burn(0, 60, 1000, '0x')).not.to.be.reverted; 
+        await expect(mockPool.flash(wallet.address, 100, 100, '0x')).not.to.be.reverted; 
+        expect((await mockPool.globalState()).pluginConfig).to.be.eq(defaultConfig);
+      });
+    });
+
+    describe('BURN_ONLY status', async () => {
+      it('works correct', async () => {
+        await registry.setGlobalStatus(1)
+        await expect(mockPool.swapToTick(10)).to.be.revertedWithCustomError(plugin,'BurnOnly'); 
+        await expect(mockPool.mint(wallet.address, wallet.address, 0, 60, 100, '0x')).to.be.revertedWithCustomError(plugin,'BurnOnly'); 
+        await expect(mockPool.burn(0, 60, 1000, '0x')).to.not.be.reverted;
+        await expect(mockPool.flash(wallet.address, 100, 100, '0x')).to.be.revertedWithCustomError(plugin,'BurnOnly'); 
+        expect((await mockPool.globalState()).pluginConfig).to.be.eq(defaultConfig);
+      });
+    });
+
+    describe('DISABLED status', async () => {
+      it('works correct', async () => {
+        await registry.setGlobalStatus(2)
+        await expect(mockPool.swapToTick(10)).to.be.revertedWithCustomError(plugin,'PoolDisabled');
+        await expect(mockPool.mint(wallet.address, wallet.address, 0, 60, 100, '0x')).to.be.revertedWithCustomError(plugin,'PoolDisabled');
+        await expect(mockPool.burn(0, 60, 1000, '0x')).to.be.revertedWithCustomError(plugin,'PoolDisabled');
+        await expect(mockPool.flash(wallet.address, 100, 100, '0x')).to.be.revertedWithCustomError(plugin,'PoolDisabled');
+        expect((await mockPool.globalState()).pluginConfig).to.be.eq(defaultConfig);
+
+      });
+    });
   })
 
   describe('AlgebaraSecurityPlugin external methods', () => {
+     
+    it('set registry contract works correct', async () => {
+      await plugin.setSecurityRegistry(ZeroAddress);
+      await expect(plugin.setSecurityRegistry(registry)).to.emit(plugin, 'SecurityRegistry');
+      expect(await plugin.getSecurityRegistry()).to.be.eq(registry);
+    });
+
+    it('only owner can set registry address', async () => {
+      await expect(plugin.connect(other).setSecurityRegistry(ZeroAddress)).to.be.reverted;
+    });
 
   });
 
   describe('#SecurtityRegistry', () => {
 
+    describe('#setPoolStatus', async () => {
+        it('works correct', async () => {
+          await registry.setPoolsStatus([mockPool], [1]);
+          expect(await registry.poolStatus(mockPool)).to.be.eq(1);
+          await registry.setPoolsStatus([mockPool], [2]);
+          expect(await registry.poolStatus(mockPool)).to.be.eq(2);
+          await registry.setPoolsStatus([mockPool], [0]);
+          expect(await registry.poolStatus(mockPool)).to.be.eq(0);
+        });
+
+      it('only owner can set all pool status', async () => {
+        await expect(registry.connect(other).setPoolsStatus([mockPool], [1])).to.be.reverted
+        await mockFactory.grantRole(await registry.GUARD(), other.address);
+        await expect(registry.connect(other).setPoolsStatus([mockPool], [0])).to.be.reverted
+        await expect(registry.connect(other).setPoolsStatus([mockPool], [1])).to.be.reverted
+      });
+
+      it('address with guard role can set DISABLED pool status', async () => {
+        await mockFactory.grantRole(await registry.GUARD(), other.address);
+        await expect(registry.connect(other).setPoolsStatus([mockPool], [2])).to.emit(registry, 'PoolStatus');
+        expect(await registry.poolStatus(mockPool)).to.be.eq(2);
+      });
+    });
+
+
+    describe('#setGlobalStatus', async () => {
+        it('works correct', async () => {
+          await registry.setGlobalStatus(1);
+          expect(await registry.globalStatus()).to.be.eq(1);
+          await registry.setGlobalStatus(2);
+          expect(await registry.globalStatus()).to.be.eq(2);
+          await registry.setGlobalStatus(0);
+          expect(await registry.globalStatus()).to.be.eq(0);
+        });
+
+        it('only owner can set all pool status', async () => {
+          await expect(registry.connect(other).setGlobalStatus(1)).to.be.reverted
+          await mockFactory.grantRole(await registry.GUARD(), other.address);
+          await expect(registry.connect(other).setGlobalStatus(1)).to.be.reverted
+          await expect(registry.connect(other).setGlobalStatus(0)).to.be.reverted
+        });
+
+        it('address with guard role can set DISABLED pool status', async () => {
+          await mockFactory.grantRole(await registry.GUARD(), other.address);
+          await expect(registry.connect(other).setGlobalStatus(2)).to.emit(registry, 'GlobalStatus');
+          expect(await registry.globalStatus()).to.be.eq(2);
+        });
+    });
+
+    describe('#getPoolStatus', async () => {
+      it('pool status overrides global status, if global status is ENABLED ', async () => {
+        await registry.setGlobalStatus(0);
+        await registry.setPoolsStatus([mockPool], [1]);
+        expect(await registry.getPoolStatus(mockPool)).to.be.eq(1);
+
+        await registry.setGlobalStatus(0);
+        await registry.setPoolsStatus([mockPool], [2]);
+        expect(await registry.getPoolStatus(mockPool)).to.be.eq(2);
+      });
+
+      it('global status overrides pool status, if global status is BURN_ONLY or DISABLED ', async () => {
+        await registry.setGlobalStatus(2);
+        await registry.setPoolsStatus([mockPool], [1]);
+        expect(await registry.getPoolStatus(mockPool)).to.be.eq(2);
+
+        await registry.setGlobalStatus(1);
+        await registry.setPoolsStatus([mockPool], [2]);
+        expect(await registry.getPoolStatus(mockPool)).to.be.eq(1);
+      });
+
+  });
   });
 });
