d4e15dd620dd61ce2d5b130bb085a03179071e28
fourlen <oborin.2013@mail.ru>
2025-05-19 13:31:10 +0300

    Revert "tmp"
    This reverts commit e8ce3ddc5449015209137dab908fa6b7cbb9235c.

--- src/periphery/contracts/libraries/PoolAddress.sol
+++ src/periphery/contracts/libraries/PoolAddress.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity >=0.5.0;
 
-import 'hardhat/console.sol';
-
 /// @title Provides functions for deriving a pool address from the poolDeployer and tokens
 /// @dev Credit to Uniswap Labs under GPL-2.0-or-later license:
 /// https://github.com/Uniswap/v3-periphery
@@ -31,10 +29,6 @@ library PoolAddress {
     /// @param key The PoolKey
     /// @return pool The contract address of the Algebra pool
     function computeAddress(address poolDeployer, PoolKey memory key) internal pure returns (address pool) {
-        // console.log('???');
-        // console.log(key.token0);
-        // console.log(key.token1);
-        console.log('SISKA: ', key.token0 < key.token1);
         require(key.token0 < key.token1, 'Invalid order of tokens');
         pool = address(
             uint160(
