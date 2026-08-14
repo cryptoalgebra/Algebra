b7e120d28c2dc14814f414ae8fcdc9b81eceb60d
IliaAzhel <aje-ilia2013@yandex.by>
2026-08-14 11:52:02 +0300

    updated versions in the comments of the contracts

--- src/periphery/contracts/AlgebraCustomPoolEntryPoint.sol
+++ src/periphery/contracts/AlgebraCustomPoolEntryPoint.sol
@@ -7,7 +7,7 @@ import {IAlgebraFactory} from '@cryptoalgebra/integral-core/contracts/interfaces
 
 /// @title Algebra custom pool entry point
 /// @notice Is used to create custom pools
-/// @dev Version: Algebra Integral 1.2.2
+/// @dev Version: Algebra Integral 1.2.3
 contract AlgebraCustomPoolEntryPoint is IAlgebraCustomPoolEntryPoint {
     /// @inheritdoc IAlgebraCustomPoolEntryPoint
     address public immutable override factory;
